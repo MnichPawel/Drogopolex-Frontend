8c8083e1f72300bcb8b7de6bff522d8c31ce1415
Kamil Osak <32075064+kmlhombre@users.noreply.github.com>
2021-03-17 15:14:36 +0100

    Adding new events
    - adding new events to database

--- app/src/main/java/com/example/drogopolex/NewEventActivity.java
+++ app/src/main/java/com/example/drogopolex/NewEventActivity.java
@@ -8,9 +8,22 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class NewEventActivity extends AppCompatActivity {
     Button goToLoggedInMenuActivity;
+    Button addEvent;
+    EditText localizationInput;
+    EditText eventTypeInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +31,21 @@ public class NewEventActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_event);
 
         goToLoggedInMenuActivity = (Button) findViewById(R.id.go_back_new_event);
+        addEvent = (Button) findViewById(R.id.addNewEventButton);
+        localizationInput = (EditText) findViewById(R.id.editTextLocalization);
+        eventTypeInput = (EditText) findViewById(R.id.editTextEventType);
 
         goToLoggedInMenuActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 goToLoggedInMenuActivity();
+            }
+        });
+
+        addEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addEventRequest();
             }
         });
 
@@ -40,5 +63,51 @@ public class NewEventActivity extends AppCompatActivity {
     private void goToMainActivity() {
         Intent goToMainActivityIntent = new Intent(this, MainActivity.class);
         startActivity(goToMainActivityIntent);
+    }
+
+    private void addEventRequest() {
+        String localization = localizationInput.getText().toString();
+        String eventType = eventTypeInput.getText().toString();
+        String coordinates = "(200, 300)";
+
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("localization", localization);
+            jsonObject.put("coordinates", coordinates);
+            jsonObject.put("type", eventType);
+
+            String url = "http://10.0.2.2:5000/addReport";
+            JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObject, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    Boolean isSuccess = false;
+                    String stringError = "";
+
+                    try {
+                        isSuccess = response.getBoolean("success");
+                        stringError = response.getString("error");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (isSuccess) {
+                        Toast.makeText(getApplicationContext(), "Zgłoszenie zostało dodane", Toast.LENGTH_LONG).show();
+                        goToLoggedInMenuActivity();
+                    } else {
+                        Toast.makeText(getApplicationContext(), stringError, Toast.LENGTH_LONG).show();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            RequestSingleton.getInstance(this).addToRequestQueue(objectRequest);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
