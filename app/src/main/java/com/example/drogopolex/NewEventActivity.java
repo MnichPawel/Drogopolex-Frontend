92953da20a111bb7703d577d3f5565e9085f3685
Kamil Osak <32075064+kmlhombre@users.noreply.github.com>
2021-04-25 19:20:41 +0200

    added gps permission and get coordinates

--- app/src/main/java/com/example/drogopolex/NewEventActivity.java
+++ app/src/main/java/com/example/drogopolex/NewEventActivity.java
@@ -1,10 +1,17 @@
 package com.example.drogopolex;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,11 +26,14 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class NewEventActivity extends AppCompatActivity {
+public class NewEventActivity extends AppCompatActivity implements LocationListener {
     Button goToLoggedInMenuActivity;
     Button addEvent;
     EditText localizationInput;
     EditText eventTypeInput;
+
+    LocationManager locationManager;
+    double latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +44,19 @@ public class NewEventActivity extends AppCompatActivity {
         addEvent = (Button) findViewById(R.id.addNewEventButton);
         localizationInput = (EditText) findViewById(R.id.editTextLocalization);
         eventTypeInput = (EditText) findViewById(R.id.editTextEventType);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
         goToLoggedInMenuActivity.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,12 +91,12 @@ public class NewEventActivity extends AppCompatActivity {
     private void addEventRequest() {
         String localization = localizationInput.getText().toString();
         String eventType = eventTypeInput.getText().toString();
-        String coordinates = "(200, 300)";
 
         try {
             JSONObject jsonObject = new JSONObject();
             jsonObject.put("localization", localization);
-            jsonObject.put("coordinates", coordinates);
+            jsonObject.put("longitude", longitude);
+            jsonObject.put("latitude", latitude);
             jsonObject.put("type", eventType);
 
             String url = "http://10.0.2.2:5000/addEvent";
@@ -111,5 +134,11 @@ public class NewEventActivity extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        longitude = location.getLongitude();
+        latitude = location.getLatitude();
     }
 }
