be137cf1a41a65ff662fd4e1cf98b6fc0eb9449d
Mikolaj <mikolaj.mlynarczyk@icloud.com>
2021-11-11 19:51:10 +0100

    implemented databinding in MapActivity

--- app/src/main/java/com/example/drogopolex/events/activities/MapActivity.java
+++ app/src/main/java/com/example/drogopolex/events/activities/MapActivity.java
@@ -7,9 +7,6 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Toast;
 
 import com.example.drogopolex.R;
@@ -20,7 +17,6 @@ import com.example.drogopolex.events.utils.EventsAction;
 import com.example.drogopolex.events.viewModel.MapViewModel;
 import com.example.drogopolex.listeners.SharedPreferencesHolder;
 import com.example.drogopolex.model.DrogopolexEvent;
-import com.example.drogopolex.model.LocationDetails;
 import com.example.drogopolex.model.VoteType;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -30,7 +26,6 @@ import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.regex.Matcher;
@@ -42,7 +37,6 @@ import androidx.core.app.ActivityCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.Observer;
 
 import static com.example.drogopolex.constants.AppConstant.PERMISSIONS_REQUEST_LOCATION;
 
@@ -60,19 +54,6 @@ public class MapActivity extends FragmentActivity
 
     boolean firstLocalizationUpdateLoaded = false;
 
-    private Animation rightwardsOpenAnimation;// = AnimationUtils.loadAnimation(this,R.anim.rightwards_open_anim);
-    private Animation rightwardsCloseAnimation;// = AnimationUtils.loadAnimation(this,R.anim.rightwards_close_anim);
-    private Animation buttonJiggleStart;// = AnimationUtils.loadAnimation(this,R.anim.button_jiggle_start);
-    private Animation buttonJiggleEnd;// = AnimationUtils.loadAnimation(this,R.anim.button_jiggle_end);
-
-    private boolean clickedAdd = false; //czy menu dodawania zdarzenia ma byc widoczne czy nie
-    private boolean switchAllChosen = true; //czy widzimy wszystkie zdarzenia czy tylko subskrypcje
-    private FloatingActionButton addEventButt;
-    private FloatingActionButton eventType1;
-    private FloatingActionButton eventType2;
-    private FloatingActionButton menuButt;
-    private FloatingActionButton allEventsButt;
-    private FloatingActionButton onlySubsButt;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -82,12 +63,9 @@ public class MapActivity extends FragmentActivity
         activityMapBinding.getViewModel().onSuccessListener = this;
         activityMapBinding.getViewModel().sharedPreferencesHolder = this;
 
-        activityMapBinding.getViewModel().getAction().observe(this, new Observer<EventsAction>() {
-            @Override
-            public void onChanged(EventsAction eventsAction) {
-                if (eventsAction != null) {
-                    handleAction(eventsAction);
-                }
+        activityMapBinding.getViewModel().getAction().observe(this, eventsAction -> {
+            if (eventsAction != null) {
+                handleAction(eventsAction);
             }
         });
 
@@ -95,13 +73,6 @@ public class MapActivity extends FragmentActivity
                 .findFragmentById(R.id.map);
 
         mapFragment.getMapAsync(this);
-
-        rightwardsOpenAnimation = AnimationUtils.loadAnimation(this,R.anim.rightwards_open_anim);
-        rightwardsCloseAnimation = AnimationUtils.loadAnimation(this,R.anim.rightwards_close_anim);
-        buttonJiggleStart = AnimationUtils.loadAnimation(this,R.anim.button_jiggle_start);
-        buttonJiggleEnd = AnimationUtils.loadAnimation(this,R.anim.button_jiggle_end);
-
-        generateButtonsOnClickListeners();
     }
 
     private void handleAction(EventsAction eventsAction) {
@@ -151,19 +122,16 @@ public class MapActivity extends FragmentActivity
     }
 
     private void requestLocationUpdates() {
-        activityMapBinding.getViewModel().getLocationLiveData().observe(this, new Observer<LocationDetails>() {
-            @Override
-            public void onChanged(LocationDetails locationDetails) {
-                activityMapBinding.getViewModel().onLocationChanged(locationDetails);
+        activityMapBinding.getViewModel().getLocationLiveData().observe(this, locationDetails -> {
+            activityMapBinding.getViewModel().onLocationChanged(locationDetails);
 
-                if (!firstLocalizationUpdateLoaded) {
-                    firstLocalizationUpdateLoaded = true;
-                    LatLng location = new LatLng(
-                            Double.parseDouble(locationDetails.getLatitude()),
-                            Double.parseDouble(locationDetails.getLongitude()));
-                    Toast.makeText(getApplicationContext(), location.latitude + " - " + location.longitude, Toast.LENGTH_SHORT).show();
-                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 17));
-                }
+            if (!firstLocalizationUpdateLoaded) {
+                firstLocalizationUpdateLoaded = true;
+                LatLng location = new LatLng(
+                        Double.parseDouble(locationDetails.getLatitude()),
+                        Double.parseDouble(locationDetails.getLongitude()));
+                Toast.makeText(getApplicationContext(), location.latitude + " - " + location.longitude, Toast.LENGTH_SHORT).show();
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 17));
             }
         });
     }
@@ -175,44 +143,35 @@ public class MapActivity extends FragmentActivity
 
     @Override
     public void onSuccess(LiveData<EventsResponse> eventsResponseLiveData) {
-        eventsResponseLiveData.observe(this, new Observer<EventsResponse>() {
-            @Override
-            public void onChanged(EventsResponse eventsResponse) {
-                if(eventsResponse != null) {
-                    eventListData.clear();
-                    eventsResponse.getEvents()
-                            .forEach(event -> {
-                                VoteType userVoteType;
-                                if("1".equals(event.getUserVote())) userVoteType = VoteType.UPVOTED;
-                                else if("-1".equals(event.getUserVote())) userVoteType = VoteType.DOWNVOTED;
-                                else userVoteType = VoteType.NO_VOTE;
+        eventsResponseLiveData.observe(this, eventsResponse -> {
+            if(eventsResponse != null) {
+                eventListData.clear();
+                eventsResponse.getEvents()
+                        .forEach(event -> {
+                            VoteType userVoteType;
+                            if("1".equals(event.getUserVote())) userVoteType = VoteType.UPVOTED;
+                            else if("-1".equals(event.getUserVote())) userVoteType = VoteType.DOWNVOTED;
+                            else userVoteType = VoteType.NO_VOTE;
 
-                                LatLng coordinates = parseCoordinatesString(event.getCoordinates());
+                            LatLng coordinates = parseCoordinatesString(event.getCoordinates());
 
-                                eventListData.add(new DrogopolexEvent(
-                                        event.getType(),
-                                        event.getCountry(),
-                                        event.getStreet(),
-                                        Integer.parseInt(event.getId()),
-                                        coordinates,
-                                        Integer.parseInt(event.getValueOfVotes()),
-                                        userVoteType
-                                ));
+                            eventListData.add(new DrogopolexEvent(
+                                    event.getType(),
+                                    event.getCountry(),
+                                    event.getStreet(),
+                                    Integer.parseInt(event.getId()),
+                                    coordinates,
+                                    Integer.parseInt(event.getValueOfVotes()),
+                                    userVoteType
+                            ));
 
-                                map.addMarker(new MarkerOptions().position(coordinates).title(event.getType()));
-                            });
+                            map.addMarker(new MarkerOptions().position(coordinates).title(event.getType()));
+                        });
 
-                } else {
-                    Toast.makeText(MapActivity.this, "Nie udało się przetworzyć odpowiedzi.", Toast.LENGTH_SHORT).show();
-                }
+            } else {
+                Toast.makeText(MapActivity.this, "Nie udało się przetworzyć odpowiedzi.", Toast.LENGTH_SHORT).show();
             }
         });
-    }
-
-    private void addMapMarker(String name, LatLng latLng) {
-        LatLng maharashtra = new LatLng(19.204847, 73.039494);
-        map.addMarker(new MarkerOptions().position(maharashtra).title("Maharashtra"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(maharashtra));
     }
 
     private LatLng parseCoordinatesString(String latLngString) {
@@ -237,97 +196,5 @@ public class MapActivity extends FragmentActivity
         LatLng currentUserLatLng = new LatLng(location.getLatitude(), location.getLongitude());
         Toast.makeText(getApplicationContext(), "onmlc " + currentUserLatLng.latitude + " - " + currentUserLatLng.longitude, Toast.LENGTH_SHORT).show();
         map.moveCamera(CameraUpdateFactory.newLatLng(currentUserLatLng));
-    }
-    private void generateButtonsOnClickListeners(){
-
-        addEventButt = findViewById(R.id.dodajZdarzenie);
-        addEventButt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setVisibility(clickedAdd);
-                setAnimation(clickedAdd);
-                clickedAdd = !clickedAdd;
-            }
-        });
-
-        eventType1 = findViewById(R.id.zdarzenie1);
-        eventType1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Zdarzenie1", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        eventType2 = findViewById(R.id.zdarzenie2);
-        eventType2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Zdarzenie2", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
-
-        //menu
-        menuButt = findViewById(R.id.menu);
-        menuButt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "menu glowne", Toast.LENGTH_SHORT).show();
-            }
-        });
-        //switch (2 buttons)
-        allEventsButt = findViewById(R.id.allEventsShown);
-        allEventsButt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(allEventsButt.getVisibility()==View.VISIBLE) {
-                    switchAllChosen = !switchAllChosen;
-                    setVisibilitySwitch(switchAllChosen);
-
-                    Toast.makeText(getApplicationContext(), "all Events ", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-        onlySubsButt = findViewById(R.id.onlySubscriptionsShown);
-        onlySubsButt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(onlySubsButt.getVisibility()==View.VISIBLE) {
-                    switchAllChosen = !switchAllChosen;
-                    setVisibilitySwitch(switchAllChosen);
-                    Toast.makeText(getApplicationContext(), "only Subs", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-    private void setVisibility(boolean clicked){
-        if(clicked){
-            eventType1.setVisibility(View.GONE);
-            eventType2.setVisibility(View.GONE);
-        }else{
-            eventType1.setVisibility(View.VISIBLE);
-            eventType2.setVisibility(View.VISIBLE);
-        }
-    }
-    private void setVisibilitySwitch(boolean clicked){ //setVisibility for switch buttons
-        if(clicked){
-            onlySubsButt.setVisibility(View.GONE);
-            allEventsButt.setVisibility(View.VISIBLE);
-        }else{
-            onlySubsButt.setVisibility(View.VISIBLE);
-            allEventsButt.setVisibility(View.GONE);
-        }
-    }
-    private void setAnimation(boolean clicked){
-        if(clicked){
-            eventType1.startAnimation(rightwardsCloseAnimation);
-            eventType2.startAnimation(rightwardsCloseAnimation);
-            addEventButt.startAnimation(buttonJiggleEnd);
-        }else{
-            eventType1.startAnimation(rightwardsOpenAnimation);
-            eventType2.startAnimation(rightwardsOpenAnimation);
-            addEventButt.startAnimation(buttonJiggleStart);
-        }
     }
 }
--- app/src/main/java/com/example/drogopolex/events/viewModel/MapViewModel.java
+++ app/src/main/java/com/example/drogopolex/events/viewModel/MapViewModel.java
@@ -2,7 +2,12 @@ package com.example.drogopolex.events.viewModel;
 
 import android.app.Application;
 import android.content.SharedPreferences;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
+import com.example.drogopolex.R;
 import com.example.drogopolex.data.network.response.EventsResponse;
 import com.example.drogopolex.data.repositories.EventsRepository;
 import com.example.drogopolex.events.utils.EventsAction;
@@ -10,13 +15,20 @@ import com.example.drogopolex.listeners.SharedPreferencesHolder;
 import com.example.drogopolex.model.LocationDetails;
 import com.example.drogopolex.services.LocationLiveData;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.Bindable;
+import androidx.databinding.Observable;
+import androidx.databinding.PropertyChangeRegistry;
+import androidx.databinding.library.baseAdapters.BR;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
-public class MapViewModel extends AndroidViewModel {
+public class MapViewModel extends AndroidViewModel implements Observable {
+    private PropertyChangeRegistry callbacks = new PropertyChangeRegistry();
+
     private MutableLiveData<EventsAction> mAction = new MutableLiveData<>();
     private LiveData<EventsResponse> eventsLiveData = new MutableLiveData<>();
     private LocationLiveData locationLiveData;
@@ -26,10 +38,20 @@ public class MapViewModel extends AndroidViewModel {
 
     private EventsRepository eventsRepository;
 
+    public int event1ButtonVisibility = View.VISIBLE;
+    public int event2ButtonVisibility = View.VISIBLE;
+
+    public boolean addEventButtonClicked = false;
+    private boolean isOnlySubs = false;
+
+    private final Animation flipButtonOut;
+
     public MapViewModel(@NonNull Application application) {
         super(application);
         locationLiveData = new LocationLiveData(application);
         eventsRepository = new EventsRepository();
+
+        flipButtonOut = AnimationUtils.loadAnimation(application.getApplicationContext(), R.anim.flip_button_out);
     }
 
     public LiveData<EventsAction> getAction() {
@@ -46,5 +68,69 @@ public class MapViewModel extends AndroidViewModel {
         String token = sharedPreferences.getString("token", "");
         eventsLiveData = eventsRepository.getEventsFromUserArea(userId, token, location.getLatitude(), location.getLongitude());
         onSuccessListener.onSuccess(eventsLiveData);
+    }
+
+    public void onAddEventClicked() {
+        Log.d("MAP_BINDING_DEBUG", "onAddEventClicked");
+        addEventButtonClicked = !addEventButtonClicked;
+        notifyPropertyChanged(BR.addEventButtonClicked);
+    }
+
+    public void onEvent1Clicked() {
+//        to implement
+    }
+
+    public void onEvent2Clicked() {
+//        to implement
+    }
+
+    public void onMenuClicked() {
+//        to implement
+    }
+
+    public void onShowAllEventsClicked(View view) {
+        Log.d("SHOW_ALL_EVENTS", "saec  " + view.getVisibility());
+        view.startAnimation(flipButtonOut);
+        if(isOnlySubs) {
+            ((FloatingActionButton) view).setImageResource(R.drawable.ic_switch_left);
+        } else {
+            ((FloatingActionButton) view).setImageResource(R.drawable.ic_switch_right);
+        }
+        isOnlySubs = !isOnlySubs;
+    }
+
+    @Bindable
+    public boolean isAddEventButtonClicked() {
+        return addEventButtonClicked;
+    }
+
+    @Override
+    public void addOnPropertyChangedCallback(
+            Observable.OnPropertyChangedCallback callback) {
+        callbacks.add(callback);
+    }
+
+    @Override
+    public void removeOnPropertyChangedCallback(
+            Observable.OnPropertyChangedCallback callback) {
+        callbacks.remove(callback);
+    }
+
+    /**
+     * Notifies observers that all properties of this instance have changed.
+     */
+    void notifyChange() {
+        callbacks.notifyCallbacks(this, 0, null);
+    }
+
+    /**
+     * Notifies observers that a specific property has changed. The getter for the
+     * property that changes should be marked with the @Bindable annotation to
+     * generate a field in the BR class to be used as the fieldId parameter.
+     *
+     * @param fieldId The generated BR id for the Bindable field.
+     */
+    void notifyPropertyChanged(int fieldId) {
+        callbacks.notifyCallbacks(this, fieldId, null);
     }
 }
